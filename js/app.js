a9ad0ec52a3b030a22e620b7cdf771ad3bd43275
ethan375 <ethanebel1@gmail.com>
2020-02-07 10:22:43 -0700

    setup grid, static header, nav and welcome section

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// const navBar = document.getElementById('nav');
+// let sticky = navBar.offsetTop;
+
+// let determineSticky = () => {
+//     if(window.pageYOffset >= sticky) {
+//         navBar.classList.add('nav__sticky');
+//     } else {
+//         navBar.classList.remove('nav__sticky')
+//     }
+// }
+
+// window.onscroll = determineSticky
