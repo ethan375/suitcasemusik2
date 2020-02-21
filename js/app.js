7a427f17062d7d35e09c655281a6f3efb745c70f
ethan375 <ethanebel1@gmail.com>
2020-02-20 20:34:03 -0700

    built basic contact us page

--- js/app.js
+++ js/app.js
@@ -10,3 +10,37 @@
 // }
 
 // window.onscroll = determineSticky
+
+function contactUs() {
+    location.replace('http://localhost:8080/views/contact.html')
+}
+
+
+let handleFormSubmit = (e) => {
+    e.preventDefault;
+    let form = document.getElementsByClassName('contact__form');
+    let contactEmail = form.child[0];
+    let subject = form.child[1];
+    let message = form.child[2];
+
+    const data = {
+        contact: contactEmail,
+        subject: subject,
+        message: message
+    };
+
+    data = JSON.stringify(data);
+
+    sendEmail(data)
+}
+
+let sendEmail = (emailContent) => {
+    fetch('https://www.herokuapp.suitcasebackend.com/contact', {
+        method: 'POST',
+        headers: {
+            contentType: 'application/json',
+
+        },
+        data: emailContent
+    }).then(res => console.log(res))
+}
