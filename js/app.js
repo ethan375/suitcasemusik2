1fc8ad2586d35e968c1cdb19d2e725bee05885d6
ethan375 <ethanebel1@gmail.com>
2020-02-24 23:27:34 -0700

    handled contact us button in header functionality with js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
 // window.onscroll = determineSticky
 
 function contactUs() {
-    location.replace('http://localhost:8080/views/contact.html')
+    location.replace('http://www.zeroequalsoneequalsinfinity/contact.html')
 }
 
 
